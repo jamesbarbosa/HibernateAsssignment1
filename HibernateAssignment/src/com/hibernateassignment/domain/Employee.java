e86344792651cde9be663e8c55ca9b2536d80d7c
James Barbosa <james.barbosa@orangeandbronze.com>
2011-05-04 19:28:00 +0800

    Employee edit 1

--- HibernateAssignment/src/com/hibernateassignment/domain/Employee.java
+++ HibernateAssignment/src/com/hibernateassignment/domain/Employee.java
@@ -1,5 +1,34 @@
 package com.hibernateassignment.domain;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int employeeID;
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	
+	
 }
